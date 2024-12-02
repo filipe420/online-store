cff0fb9bd22449e37845434d0da2fac87c511fde
DevJefersonAbreu <devjefersonabreu10@gmail.com>
2024-12-02 15:52:40 -0300

    "Atualização"

--- src/app/(pages)/(home)/styles.ts
+++ src/app/(pages)/(home)/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  min-height: 100vh; /* Altura mínima da página */
+  min-height: 100vh; 
   background-color: #4C9A9A;
 
   header {
@@ -11,7 +11,7 @@ export const Container = styled.div`
   }
 
   main {
-    flex-grow: 1; /* Garante que o conteúdo ocupe o espaço disponível */
+    flex-grow: 1; 
   }
 
   footer {
